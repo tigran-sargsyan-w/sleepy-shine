0a7a178e2e8155ee7b738738d175b583d76df3f5
Tigran Sargsyan <tigran.sargsyan.w@gmail.com>
2023-12-19 21:17:20 +0100

    - feat : tmp & plan

---
+++ sleepy-shine/Assets/Scripts/FlashLightManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class FlashLightManager : MonoBehaviour
+{
+    //Check if device is Android
+    
+    //Check if Device han a flashlight
+    
+    //Check if app has a permission
+    
+    //Check device sdk version
+    
+    //Flashlight Enable
+    
+    //Flashlight Disable
+}
