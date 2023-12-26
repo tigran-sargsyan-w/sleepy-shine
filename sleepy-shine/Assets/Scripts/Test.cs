6fb3b07579c856703d84e1252497e53db905d2d7
Tigran Sargsyan <tigran.sargsyan.w@gmail.com>
2023-12-26 21:13:39 +0100

    - feat : sample scene & plugin

--- sleepy-shine/Assets/Scripts/Test.cs
+++ sleepy-shine/Assets/Scripts/Test.cs
@@ -1,14 +1,56 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Test : MonoBehaviour
 {
-    public void OnTurnOn()
-    {
-        Flashlight.Enable();
-    }
+    public Button TimerButton;
+    public Button FlashlightButton;
+    public Button BackgroundChangeButton;
+    public Button SettingsButton;
     
-    public void OnTurnOff()
+    private bool isFlashlightOff = true;
+    
+    private void Start()
     {
-        Flashlight.Disable();
+        FlashlightButton.onClick.AddListener(OnFlashlightButtonClick);
+        TimerButton.onClick.AddListener(OnTimerButtonClick);
+        BackgroundChangeButton.onClick.AddListener(OnBackgroundChangeButtonClick);
+        SettingsButton.onClick.AddListener(OnSettingsButtonClick);
     }
+
+    private void OnTimerButtonClick()
+    {
+        Debug.Log("Timer button clicked");
+    }
+
+    private void OnFlashlightButtonClick()
+    {
+        if (isFlashlightOff)
+        {
+#if PLATFORM_ANDROID
+            Flashlight.Enable();
+#endif
+            Debug.Log("Flashlight enabled");
+            isFlashlightOff = false;
+        }
+        else
+        {
+#if PLATFORM_ANDROID
+            Flashlight.Disable();
+#endif
+            Debug.Log("Flashlight disabled");
+            isFlashlightOff = true;
+        }
+    }
+
+    private void OnBackgroundChangeButtonClick()
+    {
+        Debug.Log("Background change button clicked");
+    }
+
+    private void OnSettingsButtonClick()
+    {
+        Debug.Log("Settings button clicked");
+    }
+
 }
